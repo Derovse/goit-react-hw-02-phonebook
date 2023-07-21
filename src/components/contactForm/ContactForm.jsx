8ec9997ab9d6ffe9bd92bb11ba53b4d1cfe37e82
Derovse <sashamartunyk2004@gmail.com>
2023-07-21 17:39:11 +0300

    в
    в

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -16,7 +16,7 @@ const ContactForm = ({ addContact }) => {
       <input
         type="text"
         name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        pattern="^[a-zA-Zа-яА-Я]+([' -][a-zA-Zа-яА-Я ])*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         value={name}
         onChange={e => setName(e.target.value)}
@@ -25,7 +25,7 @@ const ContactForm = ({ addContact }) => {
       <input
         type="tel"
         name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        pattern="\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         value={number}
         onChange={e => setNumber(e.target.value)}
